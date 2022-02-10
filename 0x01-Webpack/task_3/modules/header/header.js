036eaca50fd4e55650c38eb9d1de3ed30f2d7de4
santiagopinzond <santiagopinzon456@gmail.com>
2022-02-09 21:26:46 -0500

    add projects rest

---
+++ 0x01-Webpack/task_3/modules/header/header.js
@@ -0,0 +1,8 @@
+import $ from 'jquery';
+import img from "../../assets/holberton-logo.jpg"
+import "../footer/footer.css";
+
+$('body').append("<div id='logo'></div>");
+$('body').append("<h1>Holberton Dashboard</h1>");
+
+console.log("Init header");
